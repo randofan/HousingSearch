42181155608f10ebcba456fa658bfece5c0bd1bf
randofan <davidsong.98075@gmail.com>
2022-08-18 16:33:59 -0700

    fixed compile issue for leaflet

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {MapContainer} from 'react-leaflet'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MapContainer />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
