85bf2e8167be436763ebcdbe0a40db366ba021fc
Jenny Peng <jennypscale@gmail.com>
2022-08-18 17:21:00 -0700

    basic map page setup

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {MapContainer} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Home from './Home';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MapContainer />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home></Home>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </header>
     </div>
   );
---
+++ react-app/src/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles/home.css';
+
+export default function Home() {
+    return (
+        <div className="search-bar">
+            <input type="text" className="search" placeholder="Enter a city, state, or zip code."></input>
+            <button>Search</button>
+        </div>
+    )
+}
