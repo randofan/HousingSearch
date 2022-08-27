4769a51e82a4599b310fc5523b9add5da7ae2c1f
Jenny Peng <jennypscale@gmail.com>
2022-08-26 17:29:17 -0700

    rough filter ui

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,23 +2,14 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Home from './Home';
 import './styles/App.css';
+import MapManager from './MapManager';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Home></Home>
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+        <MapManager></MapManager>
       </header>
     </div>
   );
--- react-app/src/Home.tsx
+++ react-app/src/Home.tsx
@@ -1,11 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/home.css';
 
 export default function Home() {
     return (
-        <div className="search-bar">
+        <div className="bar-container">
+            <SearchBar></SearchBar>
+            <FilterBar>
+                <FilterBarItem name="Beds">
+                    <Dropdown><p>hi</p></Dropdown>
+                </FilterBarItem>
+                <FilterBarItem name="Price">
+                    <Dropdown></Dropdown>
+                </FilterBarItem>
+                <FilterBarItem name="Type">
+                    <Dropdown></Dropdown>
+                </FilterBarItem>
+                <FilterBarItem name="More">
+                    <Dropdown></Dropdown>
+                </FilterBarItem>
+            </FilterBar>
+        </div>
+    )
+}
+
+function SearchBar() {
+    return (
+        <div className="searchbar">
             <input type="text" className="search" placeholder="Enter a city, state, or zip code."></input>
             <button className="button">Search</button>
+        </div>
+    )
+}
+
+function FilterBar(props: any) {
+    return (
+        <nav className="filterbar">
+            <ul className="filterbar-list">{props.children}</ul>
+        </nav>
+    )
+}
+
+function FilterBarItem(props: any) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <li className="filterbar-item">
+            <button onClick={() => setOpen(!open)}>{props.name}</button>
+            {open && props.children}
+        </li>
+    )
+}
+
+function Dropdown(props:any) {
+    // should pass in array of what you want??? ie. checkbox, etc.
+    // loop to render it all?
+    return (
+        <div className="dropdown">
+            {props.children}
         </div>
     )
 }
