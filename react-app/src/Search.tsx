a47c7246d8d72255226a22816f975b85145fe787
Jenny Peng <jennypscale@gmail.com>
2022-08-29 00:38:59 -0700

    basic select ui

--- react-app/src/Search.tsx
+++ react-app/src/Search.tsx
@@ -6,26 +6,10 @@ export default function Search() {
         <div className="bar-container">
             <SearchBar></SearchBar>
             <FilterBar>
-                <FilterBarItem name="Beds">
-                    <Dropdown>
-                        <DropdownSection type="checkbox" items={["hey", "dies", "bruh"]}>
-                        </DropdownSection>
-                    </Dropdown>
-                </FilterBarItem>
-                <FilterBarItem name="Price">
-                    <Dropdown></Dropdown>
-                </FilterBarItem>
-                <FilterBarItem name="Type">
-                    <Dropdown></Dropdown>
-                </FilterBarItem>
-                <FilterBarItem name="More">
-                    <Dropdown>
-                        <DropdownSection type="checkbox" name="Bathrooms" items={["1+ baths", "2+ baths", "3+ baths"]}></DropdownSection>
-                        <DropdownSection type="checkbox" name="Pet Policy" items={["Dogs", "Cats", "No Pets"]}></DropdownSection>
-                        <hr></hr>
-                        <DropdownSection type="checkbox" name="Pet Policy" items={["Dogs", "Cats", "No Pets"]}></DropdownSection>
-                    </Dropdown>
-                </FilterBarItem>
+                <FilterBarItem title="Beds" options={["Studio", "1+ Beds", "2+ Beds", "3+ Beds", "4+ Beds"]}></FilterBarItem>
+                <FilterBarItem title="Max Price" options={["Any", "$300", "$400", "$500", "$600", "$700", "$800"]}></FilterBarItem>
+                <FilterBarItem title="Type" options={["Apartment", "Townhouse", "House"]}></FilterBarItem>
+                <FilterBarItem title="More"></FilterBarItem>
             </FilterBar>
         </div>
     )
@@ -43,44 +27,30 @@ function SearchBar() {
 function FilterBar(props: any) {
     return (
         <nav className="filterbar">
-            <ul className="filterbar-list">{props.children}</ul>
+            <ul className="filterbar-list">
+                <form id="filter-form">
+                    {props.children}
+                </form>
+            </ul>
         </nav>
     )
 }
 
 function FilterBarItem(props: any) {
-    const [open, setOpen] = useState(false);
+
+    let options = [];
+    options.push(<option hidden>{props.title}</option>);
+    if (props.options != null) {
+        for (let i = 0; i < props.options.length; i++) {
+          options.push(<option>{props.options[i]}</option>);
+        }
+    }
 
     return (
         <li className="filterbar-item">
-            <button onClick={() => setOpen(!open)}>{props.name}</button>
-            {open && props.children}
+            <select className="filter-select">
+                {options}
+            </select>
         </li>
-    )
-}
-
-function Dropdown(props: any) {
-    // should pass in array of what you want??? ie. checkbox, etc.
-    // loop to render it all?
-    return (
-        <div className="dropdown">
-            {props.children}
-        </div>
-    )
-}
-
-
-
-function DropdownSection(props: any) {
-    let arr: string[] = props.items;
-    let items = [];
-    for (let i = 0; i < arr.length; i++) {
-        items.push(<label className="item-label"><input type={props.type}></input>{arr[i]}</label>);
-    }
-    return (
-        <div className="dropdown-section">
-            <h3>{props.name}</h3>
-            {items}
-        </div>
     )
 }
